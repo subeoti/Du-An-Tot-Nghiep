c54f6f1622de007762c6e21ca39979e5a8cbb1de
subeoti <tranminhhoang.2172@gmail.com>
2024-03-04 10:48:43 +0700

    SAVE CODE

---
+++ DaTa/ProductDA/ProductDA.cs
@@ -0,0 +1,38 @@
+﻿using DaTa.Model;
+using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.ComponentModel;
+using Common;
+
+namespace DaTa.ProductDA
+{
+    public class ProductDA
+    {
+
+   //     public ActionResult GetListProduct(SanPham sanpham,int? id)
+   //     {
+   //         var result = new List<SanPham>();
+			//try
+			//{
+   //             var paramList = new List<SqlParameter>
+   //             {
+   //                  new SqlParameter("@ID",sanpham.ID)
+   //             };
+   //             result = DatabaseSql.ExecuteProcToList<SanPham>("", paramList, Common.Constants.ConectionStr_DuAnTotNghiep_v1).ToList();
+
+   //         }
+			//catch (Exception)
+			//{
+   //             return new List<SanPham>();
+
+   //         }
+   //     }
+    }
+}
---
+++ WebAPI/Product/ProDuct.cs
@@ -0,0 +1,24 @@
+﻿using DaTa.Model;
+using DaTa.ProductDA;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Mvc;
+
+namespace WebAPI.Product
+{
+    public class ProDuct 
+    {
+        private ProductDA productDA = new ProductDA();
+        //public IActionResult Index()
+        //{
+        //    return View();
+
+        //}
+        //public List<SanPham> GetProduct(SanPham sanpham, int? id)
+        //{
+        //    var data = productDA.GetListProduct(sanpham, id);
+        //    return data;
+
+        //}
+    }
+  
+}
